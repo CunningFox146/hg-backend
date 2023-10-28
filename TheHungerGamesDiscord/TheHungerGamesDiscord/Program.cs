d993c98868ec4bf335d0e6c54fa8ce405fc10049
Makar Dezhemesov <dezhemesov.work@gmail.com>
2023-10-28 21:44:34 +0300

    Chore: new discord proj

--- TheHungerGamesDiscord/TheHungerGamesDiscord/Program.cs
+++ TheHungerGamesDiscord/TheHungerGamesDiscord/Program.cs
@@ -1,25 +1,36 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
 
-// Add services to the container.
+namespace TheHungerGamesDiscord;
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private DiscordSocketClient _client;
+    public static Task Main(string[] args) => new Program().MainAsync();
+
+    private async Task MainAsync()
+    {
+        
+        var builder = new ConfigurationBuilder();
+        builder.AddJsonFile("appsettings.json");
+        IConfiguration configuration = builder.Build();
+        
+        _client = new DiscordSocketClient();
+
+        _client.Log += Log;
+        
+        var token = configuration["DiscordToken"];
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        // Block this task until the program is closed.
+        await Task.Delay(-1);
+    }
+    
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
